c8dbba1831e0cfb90f5515de54dc371b5138fbca
minato <gabriel.lanziner@lanziner.at>
2020-08-29 11:28:19 +0200

    updated

--- elisp/custom-editing.el
+++ elisp/custom-editing.el
@@ -146,6 +146,7 @@
                 ("\\.epub\\'"   . nov-mode)
                 (".shrc"        . shell-script-mode)
                 (".shenv"       . shell-script-mode)
+                ("\\.xpm\\'"   . text-mode)
                 (".fvwm2rc'"    . conf-mode)
                 (".Xdefaults'"  . conf-xdefaults-mode)
                 (".Xresources'" . conf-xdefaults-mode)
