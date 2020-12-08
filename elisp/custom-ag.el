75a4e53c155dde36800126044042aed807f371e4
Gabriel Lanziner <gabriel.lanziner@lanziner.at>
2020-12-08 10:26:20 +0100

    updated

--- elisp/custom-ag.el
+++ elisp/custom-ag.el
@@ -1,22 +1,22 @@
 (require 'compile)
 
-
 (define-compilation-mode ag-mode "Ag"
   "Ag results compilation mode")
 
 (bind-keys :map ag-mode-map
            ("p"  . compilation-previous-error)
            ("n"  . compilation-next-error)
-           ("k"  . (lambda () (interactive)
-                     (let (kill-buffer-query-functions) (kill-buffer)))))
+           ("k"  . (lambda ()
+                     (interactive)
+                     (let
+                         (kill-buffer-query-functions)
+                       (kill-buffer)))))
 
 (defun ag/string (string)
   (progn
     (set (make-local-variable 'command-string)
          (concat "ag " string))
-    (compilation-start
-     command-string
-     #'ag-mode)))
+    (compilation-start command-string #'ag-mode)))
 
 (defun ag ()
   (interactive)
