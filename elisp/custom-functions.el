8fb6f20ffa4cb29418d5a10751e0f8b21985e9fe
Gabriel Lanziner <gabriel.lanziner@i-med.ac.at>
2021-03-16 14:10:07 +0100

    updated

--- elisp/custom-functions.el
+++ elisp/custom-functions.el
@@ -29,7 +29,7 @@
   (end-of-buffer))
 (defun rem ()  (interactive) (load-note ".personal/notes/reminder/reminder.org"))
 (defun n ()    (interactive) (load-note "notes.org"))
-(defun nlc ()  (interactive) (load-note ".local/notes.local"))
+(defun nlc ()  (interactive) (load-note ".local/notes.local.org"))
 (defun plan () (interactive) (load-note ".plan"))
 (defun uni ()  (interactive) (load-note ".local/uni_notes.org"))
 (defun til ()  (interactive) (load-note ".local/til_notes.org"))
