8140dc0587da215bc9eccfb0430b30c9a4744cac
minato <(none)>
2021-11-07 20:44:50 +0100

    updated

--- elisp/custom-font-mode.el
+++ elisp/custom-font-mode.el
@@ -22,36 +22,41 @@
      `(default ((t (:inherit default :height ,ttfh :width normal
                              :foundry "PfEd" :family ,family)))))))
 
-(defun pcf (&optional myfont)
+(defun pcf (&optional arg)
   (interactive)
-  (unless myfont
-    (cond
-     ((> dpi 130)
-      (setq myfont "-uw-ttyp0-medium-r-normal--18-170-75-75-c-90-iso10646-1"))
-     ((or (file-regular-p "~/.lf") (string= myfont "9x16"))
-      (setq myfont "-uw-ttyp0-medium-r-normal--16-150-75-75-c-80-iso10646-1"))
-     ((or (file-regular-p "~/.fixed") (string= myfont "6x13"))
-      (setq myfont "-misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso10646-1"))
-     ((or t (string= myfont "small"))
-      (setq myfont "-uw-ttyp0-medium-r-normal--14-130-75-75-c-70-iso10646-1"))))
+  (cond
+   ((string= arg "large")
+    (setq myfont "-uw-ttyp0-medium-r-normal--18-170-75-75-c-90-iso10646-1"))
+   ((or (string= arg "9x16") (string= arg "medium"))
+    (setq myfont "-uw-ttyp0-medium-r-normal--16-150-75-75-c-80-iso10646-1"))
+   ((string= arg "6x13")
+    (setq myfont "-misc-fixed-medium-r-semicondensed--13-120-75-75-c-60-iso10646-1"))
+   ((or (string= arg "small") t)
+    (setq myfont "-uw-ttyp0-medium-r-normal--13-120-75-75-c-70-iso10646-1")))
   (set-face-attribute 'default t :font myfont)
   (setq default-frame-alist `((font . ,myfont)))
   (set-frame-font myfont nil t))
 
+
 (defun default-font ()
   (interactive)
-  (if (file-regular-p "~/.ttf")
-      (ttf)
-    (pcf)))
+  (cond
+   ((file-regular-p "~/.ttf")
+    (ttf))
+   ((file-regular-p "~/.fixed")
+     (pcf "6x13"))
+   ((file-regular-p "~/.lf")
+    (pcf "9x16"))
+   (t
+    (if (> dpi 130)
+        (pcf "large"))
+    (pcf "small"))))
 
-(defun small-font ()
-  (interactive)
-  (cfont/set-font cfont/small))
-
-(defun 6x13 ()        (interactive) (pcf "small"))
+(defun small-font ()  (interactive) (pcf "small"))
+(defun medium-font () (interactive) (pcf "medium"))
+(defun large-font ()  (interactive) (pcf "large"))
+(defun 6x13 ()        (interactive) (pcf "6x13"))
 (defun 9x16 ()        (interactive) (pcf "9x16"))
-
-(defun medium-font () (interactive) (cfont/set-font cfont/9x16))
 
 (defun reset-fonts ()
   (interactive)
@@ -69,6 +74,7 @@
 (defalias 'menlo  'ttf)
 (defalias 'small  'small-font)
 (defalias 'medium 'medium-font)
+(defalias 'large  'large-font)
 
-(when (daemonp)
+(when (or (daemonp) (display-graphic-p))
   (default-font))
