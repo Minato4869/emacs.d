4d4a534ad042102ff0dddcf47f3c8df86a651c62
minato <(none)>
2021-11-26 07:09:12 +0100

    updated

--- elisp/custom-editing.el
+++ elisp/custom-editing.el
@@ -51,7 +51,7 @@
 
 ;; mouse
 (setq mouse-autoselect-window t)
-(xterm-mouse-mode 1)
+;;(xterm-mouse-mode 1)
 
 (defun cedit/indent-conf (offset autofill tabs &optional fill)
   (setq c-basic-offset offset
