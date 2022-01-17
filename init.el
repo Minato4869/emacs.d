74532c00cf9c58efa677f6a4e9b1f5436fad22d3
minato <(none)>
2022-01-17 08:28:23 +0100

    updated

--- init.el
+++ init.el
@@ -81,10 +81,10 @@ Errors will be logged to the buffer *Init Errors*"
 (when (my_daemonp)
   (setq confirm-kill-emacs 'yes-or-no-p)
   (load-library-wrap-error "custom-font-mode")
-  (load-library-wrap-error "custom-frame")
   (load-library-wrap-error "custom-terminal-mode")
   (load-library-wrap-error "custom-external-modes"))
 
+(load-library-wrap-error "custom-frame")
 (load-library-wrap-error "custom-theme")
 (load-library-wrap-error "custom-colours")
 (load-library-wrap-error "custom-aliases")
