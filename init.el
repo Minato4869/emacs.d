39098a10e3bdd9a6856f6c522bda4862030dccc1
minato <(none)>
2022-06-05 20:59:27 +0200

    updated

--- init.el
+++ init.el
@@ -1069,7 +1069,7 @@ making them easy to toggle.  Also, all defined keybindings can be listed here:
                          ((= dpi 109) 79)
                          ((= dpi 125) 69)
                          ((= dpi 131) 75)
-                         ((= dpi 138) 75)
+                         ((= dpi 138) 88)
                          ((= dpi 157) 73) ;; was 64
                          ((= dpi 190) 59)
                          (t           70)))
